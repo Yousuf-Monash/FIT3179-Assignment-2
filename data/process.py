0c64f20d83b823fbd89737d6ae7205e1e2e10bef
Yousuf-03 <yous0002@student.monash.edu>
2023-10-14 13:30:21 +0800

    New changes

--- data/process.py
+++ data/process.py
@@ -1,22 +1,38 @@
 import pandas as pd
 
-# Load the dataset
-sheet = "Billionaires Statistics Dataset copy.csv"
-data = pd.read_csv(sheet)
+df = pd.read_csv('Billionaires Statistics Dataset.csv')
 
-# Ensure that 'finalWorth' and 'gdp_country' columns exist in the data
-if 'finalWorth' in data.columns and 'gdp_country' in data.columns:
-    # Multiply 'finalWorth' by 1 million
-    data['finalWorth'] = data['finalWorth'] * 1_000_000
-    
-    # Remove dollar signs and commas from 'gdp_country', then convert to numerical format
-    data['gdp_country'] = pd.to_numeric(data['gdp_country'].replace({'\$': '', ',': ''}, regex=True), errors='coerce')
-    
-    # Handle NaN values resulted from conversion errors as per your requirement
-    # For example, you might fill NaN values with a placeholder or the mean GDP:
-    # data['gdp_country'].fillna(data['gdp_country'].mean(), inplace=True)
-    
-    # Save the preprocessed data back to CSV if needed
-    data.to_csv('preprocessed_billionaires_data.csv', index=False)
-else:
-    print("Columns 'finalWorth' and/or 'gdp_country' not found in the data.")
+# Assuming df is your DataFrame
+columns_to_convert = [
+    "cpi_country", "cpi_change_country", "gdp_country",
+    "gross_tertiary_education_enrollment", "gross_primary_education_enrollment_country",
+    "life_expectancy_country", "tax_revenue_country_country", "total_tax_rate_country",
+    "population_country", "latitude_country", "longitude_country"
+]
+
+# Convert columns to float, converting errors (non-convertible values) to NaN
+df[columns_to_convert] = df[columns_to_convert].apply(pd.to_numeric, errors='coerce', downcast='float')
+
+df.to_csv('Billionaires Statistics Dataset.csv', encoding='utf-8', header=True, index=False)
+
+
+pd.set_option('display.max_columns', None)
+
+# Load your data
+# Replace 'your_data.csv' with your actual file path
+df = pd.read_csv('Billionaires Statistics Dataset.csv')
+
+# Get a quick overview of the dataset
+print(df.head())  # Display the first few rows of the dataframe
+
+# Get a summary of the numerical columns
+print(df.describe())
+
+# Get info about data types, non-null counts, and memory usage
+print(df.info())
+
+# Get the number of unique values in each column
+print(df.nunique())
+
+# Get the count of missing values in each column
+print(df.isnull().sum())
